1a57de9b786a9848f53772427458ff933a7528ed
Bank <tanakhanluenam@gmail.com>
2026-02-18 18:58:35 +0700

    logic

--- Main.java
+++ Main.java
@@ -1,9 +1,8 @@
 import java.awt.Font;
-import javax.swing.UIManager; // เพิ่มตัวนี้เข้าไปครับ
+import javax.swing.UIManager;
 
 public class Main {
     public static void main(String[] args) {
-        // ตั้งค่าฟอนต์ภาษาไทยให้ระบบ UI ทั้งหมด
         UIManager.put("OptionPane.messageFont", new Font("Tahoma", Font.PLAIN, 16));
         UIManager.put("Button.font", new Font("Tahoma", Font.PLAIN, 14));
         
