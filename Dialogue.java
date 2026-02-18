c408cea8391ac8467db31f8af414104a8f11c091
Bank <tanakhanluenam@gmail.com>
2026-02-18 18:44:14 +0700

    ch layout

--- Dialogue.java
+++ Dialogue.java
@@ -1,3 +1,11 @@
 public class Dialogue {
+    public String speaker;
+    public String text;
+    public String imagePath;
     
+    public Dialogue(String speaker, String text, String imagePath) {
+        this.speaker = speaker;
+        this.text = text;
+        this.imagePath = imagePath;
+    }
 }
